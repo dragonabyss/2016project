fb50a17603b0fa3a0ac4f6ac065058b00b431b23
Dragonabyss <316507887@qq.com>
2016-07-29 19:36:00 +0800

    remove all

--- dwt3fun.m
+++
@@ -1,9 +0,0 @@
-function [LLL,LLH,LHL,LHH,HLL,HLH,HHL,HHH]=dwt3fun(data,wname)
-[L,H]=xdwtfun(data,wname);
-[LL,LH]=ydwtfun(L,wname);
-[HL,HH]=ydwtfun(H,wname);
-[LLL,LLH]=zdwtfun(LL,wname);
-[LHL,LHH]=zdwtfun(LH,wname);
-[HLL,HLH]=zdwtfun(HL,wname);
-[HHL,HHH]=zdwtfun(HH,wname);
-end
--- readme.txt
+++
@@ -1,1 +0,0 @@
-通过一维单层DWT实现了3D DWT，matlab自带的3D DWT函数为wavedec3
