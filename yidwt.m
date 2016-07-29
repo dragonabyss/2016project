02310c7c89bbcf2c0eee649fcad8da0e8b82135b
Dragonabyss <316507887@qq.com>
2016-07-29 19:24:19 +0800

    version 0

---
+++ yidwt.m
@@ -0,0 +1,18 @@
+function data = yidwt(L,H,wname)
+Ls = size(L)
+Hs = size(H)
+flag = isequal(Ls,Hs);
+if flag ~= 1
+    disp('input matrix size not equal')
+    return
+end
+matSize = size(L);
+for i = 1:matSize(1)
+    for j = 1:matSize(3)
+        L1 = L(i,:,j);
+        H1 = H(i,:,j);
+        line = idwt(L1,H1,wname);
+        data(i,:,j)=line;
+    end
+end
+end
